63717e13bab4baf5633c187a0e1eb51309b62309
Vanessa Zuñiga <76055965+Vaneluuq@users.noreply.github.com>
2021-06-30 17:03:22 -0500

    primer acercamiento funcion md-links

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const axios = require("axios");
 let absolutePathFunction = (pathUser) => (path.isAbsolute(pathUser) ? pathUser : path.resolve(pathUser));
 
 // Se lee el documento md primero identificando si es un archivo o un directorio
-let readFiles = (pathUser) => {
-  const pathAbsolute= absolutePathFunction(pathUser);
+let readFiles = (pathAbsolute) => {
+  // const pathAbsolute= absolutePathFunction(pathUser);
   let arrayMdFiles = [];
     if(fs.lstatSync(pathAbsolute).isFile()){    
       if(path.extname(pathAbsolute) == ".md"){
@@ -31,118 +31,116 @@ let readFiles = (pathUser) => {
   return arrayMdFiles;
 };
 
-// se obtienen los links del archivo 
 
-const getLinks = (pathMd) => {
-  new Promise ((res, rej)=> {
+  const getLinks = (pathMd) => {
     let objLinks = [];
-    let filesMdArray = readFiles(pathMd);
-    if(err){
-      rej(new Error(`${err.code}, verifica el path`));
-    }else{
-      filesMdArray.forEach((file) => {
-        const links = markdownLinkExtractor(file.data, true);
-         res(links.forEach(link =>{   
-           objLinks.push({
-             href: link.href,
-             text: link.text,
-             file: file.route
-             });
-          }));
-        });
-      }
-    })
-    console.log(objLinks)
+    let filesMdArray = pathMd
+    filesMdArray.forEach((file) => {
+     const links = markdownLinkExtractor(file.data, true);
+      links.forEach(link =>{   
+        objLinks.push({
+          href: link.href,
+          text: link.text,
+          file: file.route
+          });
+       });
+     });
+    return objLinks;
   };
 
-const validarLinks = (arrayLink) => {
+
+
+const checkLinks = (arrayLink) => {
     const links = arrayLink
-      links.forEach((link)=> {
-        axios
+     const arrayObj = links.map((link)=> {
+       return axios
         .get(link.href)
         .then(response => {
           if (response.status >= 200 && response.status < 300) {   //si la respuesta se encuentra en este rango la respuesta es satisfactoria
-           console.log({StatusText: response.statusText, Status: response.status}) 
+           return ({...link, StatusText: response.statusText, Status: response.status})
           }else{
-           console.log({StatusText: "Fail", status: response.status})
+           return ({...link, StatusText: "Fail", status: response.status})
           } 
         }) 
         .catch(e => {
-            console.log({StatusText: "Fail", status: e.response.status})
+           return ({...link, StatusText: "Fail", status: e.response.status})
         });
      });
+     return Promise.all(arrayObj)
    };
 
 
-
-const mdLinks = (route, option) => {
+const mdLinks = (route, {validate}) => {
   return new Promise ((res, rej) => {
-//1. verificar ingreso de ruta o ingreso de ruta y opciones 
-
+    if(!fs.existsSync(route)){
+       rej(new Error("path does not exist"))
+    }
+    const routeAbs = absolutePathFunction(route)
+    const readFileMd = readFiles(routeAbs)
+    if(validate==false){
+      let links = getLinks(readFileMd)
+       res(links)
+    }else{
+      let links = getLinks(readFileMd);
+      checkLinks(links)
+      .then(obj=> { res(obj)})
+    }
   })
 }
 
 
-const arrayLinks = [
-  {
-    href: 'https://docs.npmjs.com/misc/scri',
-    text: 'Configuración de npm-scripts',
-    file: 'C:\\Users\\usuario\\Desktop\\Laboratoria\\BOG002-md-links\\directorio\\read.md'
-  }, 
-  {
-    href: 'https://nodejs.org/docs/latest-v0.10.x/api/modules.html',
-    text: '(CommonJS)',
-    file: 'C:\\Users\\usuario\\Desktop\\Laboratoria\\BOG002-md-links\\directorio\\read.md'
-  },
-  {
-    href: 'https://docs.npmjs.com/files/package.json',
-    text: 'Configuración de package.json.',
-    file: 'C:\\Users\\usuario\\Desktop\\Laboratoria\\BOG002-md-links\\directorio\\read.md'
-  }
-]
-
-
-validarLinks(arrayLinks) 
+mdLinks("directorio", { validate: true }) 
 
 
 
 
+// const arrayLinks = [
+//   {
+//     href: 'https://docs.npmjs.com/misc/scri',
+//     text: 'Configuración de npm-scripts',
+//     file: 'C:\\Users\\usuario\\Desktop\\Laboratoria\\BOG002-md-links\\directorio\\read.md'
+//   }, 
+//   {
+//     href: 'https://nodejs.org/docs/latest-v0.10.x/api/modules.html',
+//     text: '(CommonJS)',
+//     file: 'C:\\Users\\usuario\\Desktop\\Laboratoria\\BOG002-md-links\\directorio\\read.md'
+//   },
+//   {
+//     href: 'https://docs.npmjs.com/files/package.json',
+//     text: 'Configuración de package.json.',
+//     file: 'C:\\Users\\usuario\\Desktop\\Laboratoria\\BOG002-md-links\\directorio\\read.md'
+//   }
+// ]
+
+
+// validarLinks(arrayLinks).then(obj=> {
+//   console.log(obj)});
 
 
 
 
-
-
-
+// se obtienen los links del archivo 
 // const getLinks = (pathMd) => {
-//   let objLinks = [];
-//   let filesMdArray = readFiles(pathMd);
-//   filesMdArray.forEach((file) => {
-//    const links = markdownLinkExtractor(file.data, true);
-//     links.forEach(link =>{   
-//       objLinks.push({
-//         href: link.href,
-//         text: link.text,
-//         file: file.route
+//   new Promise ((res, rej)=> {
+//     let objLinks = [];
+//     let filesMdArray = readFiles(pathMd);
+//     if(err){
+//       rej(new Error(`${err.code}, verifica el path`));
+//     }else{
+//       filesMdArray.forEach((file) => {
+//         const links = markdownLinkExtractor(file.data, true);
+//          res(links.forEach(link =>{   
+//            objLinks.push({
+//              href: link.href,
+//              text: link.text,
+//              file: file.route
+//              });
+//           }));
 //         });
-//      });
-//    });
-//   console.log(objLinks)
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//       }
+//       console.log(objLinks)
+//     })
+//   }
 
 
 
